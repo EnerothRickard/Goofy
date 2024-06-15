51786afead375cb304f94eced761cfb0823e67a3
Rickard Eneroth <rickard.eneroth@gmail.com>
2024-06-15 11:16:16 +0200

    Loopar

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Goofing around");
+        Loopar(5);
     }
+
+    public static void Loopar(int varv) {
+        for (int i = 0; i < varv; i++) {
+            System.out.println("Varv: " + i);
+        }
+    }
+
 }
